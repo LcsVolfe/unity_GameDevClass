828fca712d45db14819e5cf05eafb79d951bec28
Lucas - Fedora <lcsvolfe@gmail.com>
2020-03-22 14:38:04 -0300

    Coliders, Phisics

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    Vector3 distCameraPlayer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        distCameraPlayer = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + distCameraPlayer;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocity = 10;
+    private Vector3 direction;
+    
+    void Update()
+    {
+        float axisX = Input.GetAxis("Horizontal");
+        float axisZ = Input.GetAxis("Vertical");
+
+        direction = new Vector3(axisX, 0, axisZ);
+
+        if (direction != Vector3.zero)
+        {
+            GetComponent<Animator>().SetBool("Moving", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Moving", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(
+            GetComponent<Rigidbody>().position + (direction * velocity * Time.deltaTime)
+        );
+    }
+}
