4dfdf0685e04388011c3f9d51aa809cc26473f7c
Lucas - Fedora <lcsvolfe@gmail.com>
2020-04-03 22:42:11 -0300

    cutscenes, new scenes

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    private float _teste = 1;
+    public string scene = "menu_scene";
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void closeGame()
+    {
+        Debug.Log(_teste);
+        _teste++;
+        Application.Quit();
+    }
+
+    public void openAbout()
+    {
+        SceneManager.LoadScene("about_scene");
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+    }
+    
+    public void openMenu()
+    {
+        SceneManager.LoadScene("menu_scene");
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+    }
+    
+    public void playGame()
+    {
+        SceneManager.LoadScene("first_scene");
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void chageScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Scripts/StartTextController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartTextController : MonoBehaviour
+{
+
+    public float velocity = 3f;
+    Vector3 textMoving = new Vector3(0,1);
+    private bool _bool = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("ativo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // float moveX = Input.GetAxis("Horizontal");
+        // float moveY = Input.GetAxis("Vertical");
+        // Vector3 textMoving2 = new Vector3(moveX,moveY);
+        // transform.Translate(textMoving2 * (velocity * Time.deltaTime));
+
+        // Debug.Log(gameObject.transform.position.y);
+        
+        if (gameObject.transform.position.y < 5f && _bool)
+        {
+            transform.Translate(textMoving * (velocity * Time.deltaTime));
+            
+        }
+        else
+        {
+            _bool = false;
+            Vector3 te = new Vector3(0, - 2);
+            transform.Translate(te * (velocity * Time.deltaTime));
+        }
+
+    }
+}
